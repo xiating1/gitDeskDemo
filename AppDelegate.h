cb3282b532d8b3e0be5712f86a0c1a4713d782b8
xiating1 <501430988@qq.com>
2016-12-06 10:56:05 +0800

    初始化提交

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitDeskDemo
+//
+//  Created by 夏婷 on 16/12/6.
+//  Copyright © 2016年 夏婷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitDeskDemo
+//
+//  Created by 夏婷 on 16/12/6.
+//  Copyright © 2016年 夏婷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
